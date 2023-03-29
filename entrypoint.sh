2163cc1066384e34265cf0d591cfe16b940a95bf
docktermj <michael@dockter.com>
2023-03-29 11:08:53 -0400

    #3 Savepoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,5 +13,9 @@ git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 # Make the tag.
 
 cd "${GITHUB_WORKSPACE}" || exit
+
+
+
+
 git tag -a "v${GITHUB_REF_NAME}" -m "Go module tag for version ${GITHUB_REF_NAME} by ${GITHUB_ACTOR}" ${GITHUB_WORKFLOW_SHA}
 git push origin --tags
