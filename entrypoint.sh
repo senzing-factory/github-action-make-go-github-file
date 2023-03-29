c3bc4a2786996347df1d1b423e469ebc47c32085
docktermj <michael@dockter.com>
2023-03-29 17:00:42 -0400

    #3 Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,6 +32,7 @@ pwd
 RELEASE_REPOSITORY_NAME=$(basename ${GITHUB_REPOSITORY})
 RELEASE_VERSION=${GITHUB_REF_NAME}
 RELEASE_ITERATION="0"
+RELEASE_DATE=$(date +%Y-%m-%d)
 OUTFILE="${GITHUB_WORKSPACE}/${INPUT_FILENAME}"
 
 # Write the file.
@@ -39,13 +40,14 @@ OUTFILE="${GITHUB_WORKSPACE}/${INPUT_FILENAME}"
 echo "// Created by make-go-version-file.yaml on $(date)" > ${OUTFILE}
 echo "package ${INPUT_PACKAGE}" >> ${OUTFILE}
 echo "" >> ${OUTFILE}
+echo "var githubDate            string = \"${RELEASE_DATE}\"" >> ${OUTFILE}
+echo "var githubIteration       string = \"${RELEASE_ITERATION}\"" >> ${OUTFILE}
 echo "var githubRef             string = \"${GITHUB_REF}\"" >> ${OUTFILE}
 echo "var githubRefName         string = \"${GITHUB_REF_NAME}\"" >> ${OUTFILE}
 echo "var githubRepository      string = \"${GITHUB_REPOSITORY}\"" >> ${OUTFILE}
 echo "var githubRepositoryName  string = \"${RELEASE_REPOSITORY_NAME}\"" >> ${OUTFILE}
 echo "var githubSha             string = \"${GITHUB_SHA}\"" >> ${OUTFILE}
 echo "var githubVersion         string = \"${RELEASE_VERSION}\"" >> ${OUTFILE}
-echo "var githubIteration       string = \"${RELEASE_ITERATION}\"" >> ${OUTFILE}
 echo "" >> ${OUTFILE}
 
 # Inspect the file.
@@ -57,7 +59,7 @@ cat ${OUTFILE}
 
 git add ${OUTFILE}
 git commit -m "Create ${INPUT_FILENAME} for versioned release: ${RELEASE_VERSION}"
-git push
+git push origin HEAD:${GITHUB_WORKFLOW_REF}
 
 
 # git tag -a "v${GITHUB_REF_NAME}" -m "Go module tag for version ${GITHUB_REF_NAME} by ${GITHUB_ACTOR}" ${GITHUB_WORKFLOW_SHA}
