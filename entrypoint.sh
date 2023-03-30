d9de9b0a1ec423f0ffa00cb9a9cd6c74ba00e286
docktermj <michael@dockter.com>
2023-03-30 09:23:09 -0400

    #3 Debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,12 +38,15 @@ OUTFILE="${GITHUB_WORKSPACE}/${INPUT_FILENAME}"
 # Check if file is already up-to-date.
 
 FIRST_LINE="// ${RELEASE_VERSION}"
+echo ">>> Step: 0a"
+echo "$(head -n 1 ${OUTFILE})"
 EXISTING_FIRST_LINE=$(head -n 1 ${OUTFILE})
-
+echo ">>> Step: 0b"
 if [ "${FIRST_LINE}" = "${EXISTING_FIRST_LINE}" ]; then
     echo "${FIRST_LINE}" is already up to date.
     exit 0
 fi
+echo ">>> Step: 0c"
 
 # Write the file.
 
