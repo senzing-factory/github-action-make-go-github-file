afd70d297df1cac8895b48244e181a2adcf75052
docktermj <docktermj@users.noreply.github.com>
2023-08-17 14:03:21 -0400

    make-go-github-file.yaml updated test-github-action-original.go for versioned release: 0.0.11

---
+++ test-github-action-original.go
@@ -0,0 +1,14 @@
+// 0.0.11
+// Created by make-go-github-file.yaml on Thu 17 Aug 2023 02:03:21 PM EDT
+//lint:file-ignore U1000 Ignore all unused code, it's generated
+package apackagename
+
+var (
+	githubDate           string = "2023-08-17"
+	githubIteration      string = "0"
+	githubRef            string = "refs/tags/0.0.11"
+	githubRefName        string = "0.0.11"
+	githubRepository     string = "Senzing/test-ground"
+	githubRepositoryName string = "test-ground"
+	githubVersion        string = "0.0.11"
+)
